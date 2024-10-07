1ad0ab34d07d1ab866228795ffff7a39b0857543
GaryRen <espresso1014@gmail.com>
2024-10-07 13:01:40 +1300

    initialized database

--- app/fhv/models.py
+++ app/fhv/models.py
@@ -1,4 +1,6 @@
 from datetime import date, datetime
+
+from sqlalchemy import Numeric
 from fhv.exts import db
 
 
@@ -46,9 +48,13 @@ class Staff(Person):
 class Customer(Person):
     __tablename__ = 'customer'
     id = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key=True)
-    balance = db.Column(db.Float, default=0.0)
+    balance = db.Column(Numeric(10, 2), default=0.0)
     address = db.Column(db.String(100), nullable=True)
-    max_owing = db.Column(db.Float, default=100.0)
+    max_owing = db.Column(Numeric(10, 2), default=100.0)
+    payments = db.relationship(
+        'Payment', back_populates='customer', cascade='all, delete-orphan')
+    orders = db.relationship(
+        'Order', back_populates='customer', cascade='all, delete-orphan')
 
     __mapper_args__ = {
         'polymorphic_identity': 'customer',
@@ -65,27 +71,30 @@ class Customer(Person):
 class CorporateCustomer(Customer):
     __tablename__ = 'corporate_customer'
     id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True)
-    credit_limit = db.Column(db.Float, nullable=False)
+    credit_limit = db.Column(Numeric(10, 2), nullable=False)
 
     __mapper_args__ = {
         'polymorphic_identity': 'corporate_customer',
     }
 
     def __init__(self, credit_limit: float, **kwargs):
-        # corporate customer do not need this attribute
+        # corporate customer does not need max_owing
         kwargs.pop('max_owing', None)
         super().__init__(**kwargs)
         self.credit_limit = credit_limit
 
     def __repr__(self):
-        return f"<CorporateCustomer(id={self.id}, firstname={self.firstname}, lastname={self.lastname}, balance={self.balance}>, credit_limit={self.credit_limit})"
+        return f"<CorporateCustomer(id={self.id}, firstname={self.firstname}, lastname={self.lastname}, balance={self.balance}, credit_limit={self.credit_limit})>"
 
 
 class Payment(db.Model):
     __tablename__ = 'payment'
     id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Float, nullable=False)
-    create_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    amount = db.Column(Numeric(10, 2), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    customer_id = db.Column(db.Integer, db.ForeignKey(
+        'customer.id'), nullable=False)
+    customer = db.relationship('Customer', back_populates='payments')
 
     type = db.Column(db.String(50))
     __mapper_args__ = {
@@ -93,11 +102,12 @@ class Payment(db.Model):
         'polymorphic_on': type,
     }
 
-    def __init__(self, amount: float):
+    def __init__(self, amount: float, customer_id: int):
         self.amount = amount
+        self.customer_id = customer_id
 
     def __repr__(self):
-        return f"<Payment(id={self.id}, amount={self.amount}, create_at={self.create_at})>"
+        return f"<Payment(id={self.id}, amount={self.amount}, created_at={self.created_at}, customer_id={self.customer_id})>"
 
 
 class PayByCredit(Payment):
@@ -120,8 +130,8 @@ class PayByCredit(Payment):
         self.cvv = cvv
 
     def __repr__(self):
-        return (f"<PayByCredit(id={self.id}, amount={self.amount}, create_at={self.create_at}, "
-                f"card_number={self.card_number}, cardholder={self.cardholder}, expiry={self.expiry})>")
+        return (f"<PayByCredit(id={self.id}, amount={self.amount}, created_at={self.created_at}, "
+                f"card_number={self.card_number}, cardholder={self.cardholder}, expiry={self.expiry}, customer_id={self.customer_id})>")
 
 
 class PayByDebit(Payment):
@@ -142,15 +152,15 @@ class PayByDebit(Payment):
         self.payee = payee
 
     def __repr__(self):
-        return (f"<PayByDebit(id={self.id}, amount={self.amount}, create_at={self.create_at}, "
-                f"account_number={self.account_number}, bank_name={self.bank_name}, payee={self.payee})>")
+        return (f"<PayByDebit(id={self.id}, amount={self.amount}, created_at={self.created_at}, "
+                f"account_number={self.account_number}, bank_name={self.bank_name}, payee={self.payee}, customer_id={self.customer_id})>")
 
 
 class Product(db.Model):
     __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    price = db.Column(Numeric(10, 2), nullable=False)
 
     type = db.Column(db.String(50))
     __mapper_args__ = {
@@ -158,9 +168,14 @@ class Product(db.Model):
         'polymorphic_on': type,
     }
 
+    order_items = db.relationship('OrderItem', back_populates='product')
+
     def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
+
+    def __repr__(self):
+        return f"<Product(id={self.id}, name='{self.name}', price={self.price}, type='{self.type}')>"
 
 
 class Veggies(Product):
@@ -180,6 +195,9 @@ class Veggies(Product):
         super().__init__(**kwargs)
         self.pricing_unit = pricing_unit
 
+    def __repr__(self):
+        return f"<Veggies(id={self.id}, name='{self.name}', pricing_unit='{self.pricing_unit}', price={self.price})>"
+
 
 class PremadeBox(Product):
     __tablename__ = 'premade_box'
@@ -197,6 +215,9 @@ class PremadeBox(Product):
     def __init__(self, box_size: str, **kwargs):
         super().__init__(**kwargs)
         self.box_size = box_size
+
+    def __repr__(self):
+        return f"<PremadeBox(id={self.id}, name='{self.name}', box_size='{self.box_size}', price={self.price})>"
 
 
 class PremadeBoxContent(db.Model):
@@ -216,3 +237,54 @@ class PremadeBoxContent(db.Model):
         self.premade_box_id = premade_box_id
         self.veggies_id = veggies_id
         self.quantity = quantity
+
+    def __repr__(self):
+        return f"<PremadeBoxContent(id={self.id}, premade_box_id={self.premade_box_id}, veggies_id={self.veggies_id}, quantity={self.quantity})>"
+
+
+class OrderItem(db.Model):
+    __tablename__ = 'order_item'
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Float, nullable=False)
+
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'product.id'), nullable=False)
+    product = db.relationship('Product', back_populates='order_items')
+
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    order = db.relationship('Order', back_populates='order_items')
+
+    def __init__(self, product_id: int, quantity: float, order_id: int):
+        self.product_id = product_id
+        self.quantity = quantity
+        self.order_id = order_id
+
+    def __repr__(self):
+        return f"<OrderItem(id={self.id}, product_id={self.product_id}, quantity={self.quantity}, order_id={self.order_id})>"
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey(
+        'customer.id'), nullable=False)
+    customer = db.relationship('Customer', back_populates='orders')
+
+    order_items = db.relationship(
+        'OrderItem', back_populates='order', cascade='all, delete-orphan')
+
+    order_date = db.Column(db.Date, nullable=False, default=date.today)
+    delivery_fee = db.Column(Numeric(10, 2), nullable=False, default=10.0)
+    is_delivery = db.Column(db.Boolean, nullable=False, default=False)
+    total_amount = db.Column(Numeric(10, 2), nullable=False, default=0.0)
+
+    status = db.Column(db.Enum('draft', 'pending', 'processing', 'completed', 'canceled', 'refunded', name='order_status'),
+                       nullable=False, default='draft')
+
+    def __init__(self, customer_id, is_delivery=False):
+        self.customer_id = customer_id
+        self.is_delivery = is_delivery
+
+    def __repr__(self):
+        return f"<Order(id={self.id}, customer_id={self.customer_id}, is_delivery={self.is_delivery}, status={self.status})>"
--- app/scripts/populate_db.py
+++ app/scripts/populate_db.py
@@ -4,24 +4,113 @@ python -m scripts.populate_db
 '''
 from datetime import date
 from fhv import create_app, db
-from fhv.models import Person, Staff, Customer, CorporateCustomer
+from fhv.models import Person, Staff, Customer, CorporateCustomer, Product, Veggies, PremadeBox, PremadeBoxContent
 
 app = create_app()
 
 
 def populate_data():
     with app.app_context():
+        # staff
+        staff1 = Staff(firstname='John', lastname='Doe',
+                       username='johndoe', password='john123', department='Sales')
+        staff2 = Staff(firstname='Jane', lastname='Smith',
+                       username='janesmith', password='jane123', department='Marketing')
+        staff3 = Staff(firstname='Alice', lastname='Johnson',
+                       username='alicejohnson', password='alice123', department='IT')
+        # private customer
+        customer1 = Customer(firstname='Bob', lastname='Brown',
+                             username='bobbrown', password='bob123', address='Riccarton Chch')
+        customer2 = Customer(firstname='Charlie', lastname='Taylor',
+                             username='charlietaylor', password='charlie123', address='Addington Chch')
+        customer3 = Customer(firstname='David', lastname='Walker',
+                             username='davidwalker', password='david123', address='Merivale Chch')
+        customer4 = Customer(firstname='Eva', lastname='Clark',
+                             username='evaclark', password='eva123', address='Halswell Chch')
+        customer5 = Customer(firstname='Frank', lastname='Wright',
+                             username='frankwright', password='frank123', address='Hornby Chch')
+        customer6 = Customer(firstname='Grace', lastname='Lee',
+                             username='gracelee', password='grace123', address='Ilam Chch')
+        customer7 = Customer(firstname='Henry', lastname='Scott',
+                             username='henryscott', password='henry123', address='Riccarton Chch')
+        customer8 = Customer(firstname='Isaac', lastname='Wright',
+                             username='isaacwright', password='isaac123', address='Addington Chch')
+        customer9 = Customer(firstname='Jack', lastname='Taylor',
+                             username='jacktaylor', password='jack123', address='Merivale Chch')
+        customer10 = Customer(firstname='Kelly', lastname='Johnson',
+                              username='kellyjohnson', password='kelly123', address='Halswell Chch')
+        # corporate customer
+        corporate1 = CorporateCustomer(firstname='Liam', lastname='Brown',
+                                       username='liambrown', password='liam123', address='Riccarton Chch', credit_limit=2000.0)
+        corporate2 = CorporateCustomer(firstname='Mia', lastname='Taylor',
+                                       username='miataylor', password='mia123', address='Addington Chch', credit_limit=3000.0)
+        corporate3 = CorporateCustomer(firstname='Noah', lastname='Walker',
+                                       username='noahwalker', password='noah123', address='Merivale Chch', credit_limit=4000.0)
+        corporate4 = CorporateCustomer(firstname='Olivia', lastname='Clark',
+                                       username='oliviaclark', password='olivia123', address='Halswell Chch', credit_limit=5000.0)
+        corporate5 = CorporateCustomer(firstname='Paul', lastname='Wright',
+                                       username='paulwright', password='paul123', address='Hornby Chch', credit_limit=2500.0)
+        corporate6 = CorporateCustomer(firstname='Quincy', lastname='Lee',
+                                       username='quincylee', password='quincy123', address='Ilam Chch', credit_limit=3500.0)
+        corporate7 = CorporateCustomer(firstname='Rachel', lastname='Scott',
+                                       username='rachelscott', password='rachel123', address='Riccarton Chch', credit_limit=4500.0)
+        corporate8 = CorporateCustomer(firstname='Sam', lastname='Wright',
+                                       username='samwright', password='sam123', address='Addington Chch', credit_limit=1500.0)
+        corporate9 = CorporateCustomer(firstname='Tina', lastname='Taylor',
+                                       username='tinataylor', password='tina123', address='Merivale Chch', credit_limit=3000.0)
+        corporate10 = CorporateCustomer(firstname='Uma', lastname='Johnson',
+                                        username='umajohnson', password='uma123', address='Halswell Chch', credit_limit=1000.0)
+        # veggies
+        veggies1 = Veggies(name="Carrot", price=2.50, pricing_unit="per_kilo")
+        veggies2 = Veggies(name="Tomato", price=3.00, pricing_unit="per_pack")
+        veggies3 = Veggies(name="Cucumber", price=1.50,
+                           pricing_unit="per_unit")
+        veggies4 = Veggies(name="Spinach", price=4.00, pricing_unit="per_kilo")
+        veggies5 = Veggies(name="Potato", price=1.20, pricing_unit="per_kilo")
+        veggies6 = Veggies(name="Lettuce", price=1.00, pricing_unit="per_pack")
+        veggies7 = Veggies(name="Broccoli", price=2.80,
+                           pricing_unit="per_unit")
+        veggies8 = Veggies(name="Cauliflower", price=3.20,
+                           pricing_unit="per_unit")
+        veggies9 = Veggies(name="Zucchini", price=2.00,
+                           pricing_unit="per_kilo")
+        veggies10 = Veggies(name="Onion", price=1.50, pricing_unit="per_kilo")
 
-        person1 = Person(firstname='per', lastname='son',
-                         username='person', password='person123')
-        staff1 = Staff(firstname='sta', lastname='ff',
-                       username='staff', password='staff123', date_joined=date.today(), department='departmentA')
-        customer1 = Customer(firstname='cust', lastname='mer',
-                             username='customer', password='customer123', address='Riccarton Chch')
-        corporate1 = CorporateCustomer(firstname='corpo', lastname='rate',
-                                       username='corporate', password='corporate123', address='Riccarton Chch', credit_limit=1000.0)
+        # PremadeBox
+        premade_box1 = PremadeBox(name="Box 1", price=20.00, box_size="small")
+        premade_box2 = PremadeBox(name="Box 2", price=25.00, box_size="medium")
+        premade_box3 = PremadeBox(name="Box 3", price=30.00, box_size="large")
+        premade_box4 = PremadeBox(name="Box 4", price=35.00, box_size="medium")
+        premade_box5 = PremadeBox(name="Box 5", price=40.00, box_size="large")
 
-        db.session.add_all([person1, staff1, customer1, corporate1])
+        db.session.add_all([staff1, staff2, staff3,
+                            customer1, customer2, customer3, customer4, customer5,
+                            customer6, customer7, customer8, customer9, customer10, corporate1, corporate2, corporate3,
+                            corporate4, corporate5, corporate6, corporate7, corporate8, corporate9, corporate10,
+                            veggies1, veggies2, veggies3, veggies4, veggies5,
+                            veggies6, veggies7, veggies8, veggies9, veggies10,
+                            premade_box1, premade_box2, premade_box3, premade_box4, premade_box5])
+
+        db.session.flush()
+
+        premade_box_id_1 = premade_box1.id
+        premade_box_id_2 = premade_box2.id
+        veggies_id_1 = veggies1.id
+        veggies_id_2 = veggies2.id
+
+        # PremadeBoxContent
+        premade_box_content1 = PremadeBoxContent(
+            premade_box_id=premade_box_id_1, veggies_id=veggies_id_1, quantity=2.0)
+        premade_box_content2 = PremadeBoxContent(
+            premade_box_id=premade_box_id_1, veggies_id=veggies_id_2, quantity=3.0)
+        premade_box_content3 = PremadeBoxContent(
+            premade_box_id=premade_box_id_2, veggies_id=veggies_id_1, quantity=1.0)
+        premade_box_content4 = PremadeBoxContent(
+            premade_box_id=premade_box_id_2, veggies_id=veggies_id_2, quantity=2.5)
+
+        db.session.add_all([
+            premade_box_content1, premade_box_content2, premade_box_content3,
+            premade_box_content4])
 
         db.session.commit()
 
